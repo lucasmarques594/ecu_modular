e4cc2b00d6dcb8b860edfb561988e6af096a41c4
lucasmarques594 <demordevil594@gmail.com>
2025-11-20 15:55:13 -0300

    feat(filters): add moving average filter struct definition

--- include/filters/moving_average.h
+++ include/filters/moving_average.h
@@ -4,4 +4,10 @@
 #include "../common/config.h"
 #include "../common/types.h"
 
+typedef struct {
+  sensor_valuer_t values[MOVING_AVG_WINDOW];
+  counter_t index;
+  sensor_valuer_t sum;
+} MovingAverageFilter_t;
+
 #endif
