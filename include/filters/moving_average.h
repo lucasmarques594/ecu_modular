ceb2bf0433c00522df1343e218a95289b30f4bed
lucasmarques594 <demordevil594@gmail.com>
2025-11-20 15:55:35 -0300

    feat(moving_average): add init, apply, reset and get functions
    Add function declarations for initializing, applying values to, resetting and getting values from a moving average filter

--- include/filters/moving_average.h
+++ include/filters/moving_average.h
@@ -9,5 +9,10 @@ typedef struct {
   counter_t index;
   sensor_valuer_t sum;
 } MovingAverageFilter_t;
+void MovingAverageFilter_Init(MovingAverageFilter_t *filter);
+sensor_valuer_t MovingAverageFilter_Apply(MovingAverageFilter_t *filter,
+                                          sensor_valuer_t value);
+void MovingAverageFilter_Reset(MovingAverageFilter_t *filter);
+sensor_valuer_t MovingAvarage_GetValue(const MovingAverageFilter_t *filter);
 
 #endif
