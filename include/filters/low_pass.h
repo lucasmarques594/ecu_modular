dc055c2839a918196ea4713325a1f412e8962676
lucasmarques594 <demordevil594@gmail.com>
2025-11-23 16:46:11 -0300

    feat(low_pass): add initialization and update functions for filter
    Add functions to initialize, update, reset, get value and set alpha for the low pass filter structure

--- include/filters/low_pass.h
+++ include/filters/low_pass.h
@@ -8,4 +8,11 @@ typedef struct {
   sensor_value_t alpha;
 } LowPassFilter_t;
 
+void LowPass_Init(LowPassFilter_t *filter, sensor_value_t alpha);
+sensor_value_t LowPass_Update(LowPassFilter_t *filter,
+                              sensor_value_t new_value);
+void LowPass_Reset(LowPassFilter_t *filter);
+sensor_value_t LowPass_GetValue(const LowPassFilter_t *filter);
+void LowPass_SetAlpha(LowPassFilter_t *filter, sensor_value_t alpha);
+
 #endif
