64274aa171edf40f49fe7ab98c98640742835161
lucasmarques594 <demordevil594@gmail.com>
2025-11-23 23:01:09 -0300

    feat(sensors): add Sensor_t struct to hold sensor data and filters

--- include/sensors/sensor.h
+++ include/sensors/sensor.h
@@ -6,4 +6,17 @@
 #include "../filters/low_pass.h"
 #include "../filters/moving_average.h"
 
+typedef struct {
+  const char *name;
+  sensor_value_t raw_value;
+  sensor_value_t filtered_ma;
+  sensor_value_t filtered_lp;
+  MovingAverageFilter_t ma_filter;
+  LowPassFilter_t lp_filter;
+  AlertDebounce_t alert_debounce;
+  sensor_value_t warning_threshold;
+  sensor_value_t critical_threshold;
+  bool is_low_alert;
+} Sensor_t;
+
 #endif
